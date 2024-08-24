313105815350308ec581f0d7573cf89188a9afae
Joao Victor <joaovictordesena1@gmail.com>
2024-08-24 15:25:14 -0300

    Create UserController

--- src/main/java/com/joaovictordesena/todosimple/models/user.java
+++ src/main/java/com/joaovictordesena/todosimple/models/user.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -93,6 +94,7 @@ public class User {
     }
 
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return this.tasks;
     }
