bcd412acd7822d2540bdb4f63c4d81ed4976ecb0
Joao Victor <joaovictordesena1@gmail.com>
2024-08-27 23:48:09 -0300

    Add Lombok

--- src/main/java/com/joaovictordesena/todosimple/models/user.java
+++ src/main/java/com/joaovictordesena/todosimple/models/user.java
@@ -11,16 +11,25 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class User {
     public interface CreateUser{
 
@@ -42,6 +51,7 @@ public class User {
     @Size(groups = CreateUser.class, min = 2, max = 100)
     private String username;
 
+    @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
     //verificação para criação e atualização de senha, não utilizado no username pois nessa regra de negócios não vamos
     //atualizar o usuário, só a senha
@@ -51,84 +61,8 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user")
+    @JsonProperty(access = Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
-
-
-    /**
-     * @return 
-     * 
-     */
-    public User() {
-    }
-
-    //quando alguém criar o usuário, ele vai cair nesse construtor
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-    @JsonIgnore
-    public List<Task> getTasks() {
-        return this.tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if(obj == this)
-        return true;
-      if(obj == null)
-        return false;
-      if(!(obj instanceof User))
-        return false;
-      User other = (User) obj;
-      if(this.id == null)
-        if(other.id != null)
-            return false;
-        else if(!this.id.equals(other.id))
-            return false;
-      return Objects.equals(this.id, other.id) & Objects.equals(this.username, other.username)
-            && Objects.equals(this.password, other.password);
-      
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null)  ? 0 : this.id.hashCode());
-        return result;
-    }
 
 
 
