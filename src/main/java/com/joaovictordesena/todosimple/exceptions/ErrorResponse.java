c658a8227c94ff3c9eac393ab41a0f679c8be535
Joao Victor <joaovictordesena1@gmail.com>
2024-09-02 00:46:17 -0300

    Add custom exception and handler

---
+++ src/main/java/com/joaovictordesena/todosimple/exceptions/ErrorResponse.java
@@ -0,0 +1,44 @@
+package com.joaovictordesena.todosimple.exceptions;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+    private String stackTrace;
+    private List<ValidationError> errors;
+
+    @Getter
+    @Setter
+    @RequiredArgsConstructor
+    private static class ValidationError {
+        private final String field;
+
+        private final String message;
+
+        
+    }
+
+    public void addValidationError(String field, String message) {
+        if (Objects.isNull(errors)) {
+            this.errors = new ArrayList<>();
+        }
+        this.errors.add(new ValidationError(field, message));
+    }
+
+    
+}
