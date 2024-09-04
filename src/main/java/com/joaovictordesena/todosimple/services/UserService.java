349408b594eef051a754d6cdb4b68539384d6e21
Joao Victor <joaovictordesena1@gmail.com>
2024-09-04 00:43:04 -0300

    Add password encoder and create ProfileEnum

--- src/main/java/com/joaovictordesena/todosimple/services/UserService.java
+++ src/main/java/com/joaovictordesena/todosimple/services/UserService.java
@@ -3,12 +3,16 @@ package com.joaovictordesena.todosimple.services;
 
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.joaovictordesena.todosimple.models.User;
+import com.joaovictordesena.todosimple.models.enums.ProfileEnum;
 import com.joaovictordesena.todosimple.repositories.TaskRepository;
 import com.joaovictordesena.todosimple.repositories.UserRepository;
 import com.joaovictordesena.todosimple.services.exceptions.DataBindingViolationException;
@@ -16,7 +20,8 @@ import com.joaovictordesena.todosimple.services.exceptions.ObjectNotFoundExcepti
 
 @Service
 public class UserService {
-
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     private UserRepository userRepository;
 
@@ -32,8 +37,9 @@ public class UserService {
     @Transactional
     public User create(User obj){
         obj.setId(null);
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         return obj;
 
     }
@@ -43,6 +49,7 @@ public class UserService {
 
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
